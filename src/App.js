055d57813ca264d9c2531b39d8343978677ee0f1
Viktoria <109460265+Viktoriia2@users.noreply.github.com>
2022-12-16 11:18:26 +0100

    update App js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import WeatherSearch from "./WeatherSearch";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
@@ -9,9 +9,17 @@ function App() {
       <Container fluid>
         <WeatherSearch defaultCity="Warsaw" />
       </Container>
+      <div className="work-coded d-flex justify-content-center">
+        This project was coded by Viktoria Skakovska and is{" "}
+        <a
+          href="https://github.com/Viktoriia2/weather-react-myapp"
+          target="_blank"
+        >
+          open-source on GitHub
+        </a>
+      </div>
     </div>
   );
 }
 
 export default App;
-
