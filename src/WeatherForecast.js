659a1cc635557ec319bb7d970bb0c570a8b1bc57
Viktoria <109460265+Viktoriia2@users.noreply.github.com>
2022-12-16 09:05:15 +0100

    add WeatherForecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+import "./WeatherInfo.css";
+import Row from "react-bootstrap/Row";
+
+export default function WeatherForecast (props) {
+    return (
+        <Row className="weather-forecast">
+            
+        </Row>
+    );
+}
