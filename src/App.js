533fc69301778eeb270bbe2a2f25e9ccfcb250a7
Viktoria <109460265+Viktoriia2@users.noreply.github.com>
2022-12-16 13:49:39 +0100

    update App.js + added footer

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
     <div className="App">
       <Container fluid>
         <WeatherSearch defaultCity="Warsaw" />
+        <footer className="text-left">
+          This project was coded by Viktoria Skakovska and is{" "}
+          <a
+            href="https://github.com/Viktoriia2/weather-react-myapp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
       </Container>
-      <div className="work-coded d-flex justify-content-center">
-        This project was coded by Viktoria Skakovska and is{" "}
-        <a
-          href="https://github.com/Viktoriia2/weather-react-myapp"
-          target="_blank"
-        >
-          open-source on GitHub
-        </a>
-      </div>
     </div>
   );
 }
